b9316089f578e301a5c89410b3b0b7462766e84f
Jean-Baptiste VIMORT <jb.vimort@kitware.com>
2017-06-05 12:33:56 -0400

    BUG: Correction of ITKPythonBuilds-linux.tar.zst extension
    The archive extension changed from zstd to zst

--- createPythonWheels.sh
+++ createPythonWheels.sh
@@ -4,8 +4,8 @@ wget -L https://data.kitware.com/api/v1/file/592dd8068d777f16d01e1a92/download -
 gunzip -d zstd-1.2.0-linux.tar.gz
 tar xf zstd-1.2.0-linux.tar
 
-curl -L https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/latest/ITKPythonBuilds-linux.tar.zstd -O ITKPythonBuilds-linux.tar.zstd
-./zstd-1.2.0-linux/bin/unzstd ITKPythonBuilds-linux.tar.zstd -o ITKPythonBuilds-linux.tar
+curl -L https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/latest/ITKPythonBuilds-linux.tar.zst -O ITKPythonBuilds-linux.tar.zst
+./zstd-1.2.0-linux/bin/unzstd ITKPythonBuilds-linux.tar.zst -o ITKPythonBuilds-linux.tar
 tar xf ITKPythonBuilds-linux.tar
 
 ./ITKPythonPackage/scripts/dockcross-manylinux-build-module-wheels.sh
