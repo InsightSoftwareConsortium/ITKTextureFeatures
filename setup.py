7381c29741cd3a4adf46d2ad03299acaf3777f94
Matt McCormick <matt.mccormick@kitware.com>
2020-10-05 17:05:43 -0400

    ENH: Bump Python package version to 3.3.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='itk-texturefeatures',
-    version='3.3.1',
+    version='3.3.2',
     author='Insight Software Consortium',
     author_email='community@itk.org',
     packages=['itk'],
